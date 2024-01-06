870863231121a9fdf61f0c34ff8a42563c5e216a
ecwa2020 <84256988+em1lyw4ng@users.noreply.github.com>
2024-01-06 04:36:07 -0500

    updated api version

---
+++ pages/api/generate.js
@@ -0,0 +1,31 @@
+import OpenAI from "openai";
+
+// set up openAI client
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+// initialize prompt string
+const basePromptPrefix = "";
+
+const generateAction = async (req, res) => {
+    // shows the full prompt
+    console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+    // call openAI API
+    const baseCompletion = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo-1106',
+        messages: [{
+            role: "user",
+            content: `${basePromptPrefix}${req.body.userInput}`
+        }],
+    });
+
+    // return message object
+    const basePromptOutput = baseCompletion.choices.pop();
+
+    // check that response has command and description
+    res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,48 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  // add react hooks for userInput, apiOutput, generating
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  // set userInput to updated text
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value)
+  };
+
+  // call api to generate response
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...")
+
+    // send POST request to api with data
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    // format output once received
+    const data = await response.json();
+    const { output } = data;
+
+    console.log("here is your data")
+    console.log(data)
+
+    console.log("OpenAI replied...", output.message.content)
+
+    // update state of apiOutput, isGenerating
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
   return (
     <div className="root">
       <Head>
