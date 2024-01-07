b9941c5f857b2d78991fa3cdd6c78e59efcf9b24
ecwa2020 <84256988+em1lyw4ng@users.noreply.github.com>
2024-01-06 19:44:17 -0500

    add prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,18 +6,19 @@ const openai = new OpenAI({
 });
 
 // initialize prompt string
-const basePromptPrefix = "";
+const basePromptPrefix = "You have knowledge of all terminal commands possible and also know how to piece together commands to execute complex tasks in the terminal. You will be asked by programmers with little knowledge of terminal commands about how to do something in the terminal, so you should give the command accompanied with a description of what each part does in less than 10 words.";
 
 const generateAction = async (req, res) => {
-    // shows the full prompt
-    console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
     // woohoo chat time
     const baseCompletion = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo-1106',
         messages: [{
+            role: "system",
+            content: `${basePromptPrefix}`
+        }, {
             role: "user",
-            content: `${basePromptPrefix}${req.body.userInput}`
+            content: `${req.body.userInput}`,
         }],
     });
 
